1e941f6b899fcaea39e2192c284f10f00e6b9121
erickkurniawan <erick.kurniawan@gmail.com>
2023-11-29 10:30:49 +0700

    menambahkan basetest

---
+++ TestProject1/BaseTest.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Safari;
+
+namespace TestProject1;
+
+public class BaseTest
+{
+    private IWebDriver driver;
+
+    protected IWebDriver GetDriver()
+    {
+        return driver;
+    }
+
+    [SetUp]
+    public void Setup()
+    {
+        driver = new SafariDriver();
+        driver.Manage().Window.Size = new System.Drawing.Size(1920, 1080);
+        driver.Navigate().GoToUrl("http://localhost:4200");
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        driver.Quit();
+    }
+}
--- TestProject1/SimpleTest.cs
+++ TestProject1/SimpleTest.cs
@@ -6,60 +6,28 @@ using OpenQA.Selenium.Support.UI;
 
 namespace TestProject1;
 
-public class SimpleTest
+public class SimpleTest : BaseTest
 {
-    private IWebDriver driver;
-
-    [SetUp]
-    public void Setup()
-    {
-        driver = new SafariDriver();
-        driver.Manage().Window.Size = new System.Drawing.Size(1920, 1080);
-        driver.Navigate().GoToUrl("http://localhost:4200");
-    }
-
-    [TearDown]
-    public void TearDown()
-    {
-        driver.Quit();
-    }
-
-    // [Test]
-    // public void Test_UsernameOrPasswordIsWrong()
-    // {
-    //     driver.FindElement(By.Id("user-name")).SendKeys("standard_user");
-    //     driver.FindElement(By.Id("password")).SendKeys("secret");
-    //     driver.FindElement(By.Id("login-button")).Click();
-
-    //     var pesan = driver.FindElement(By.CssSelector("h3[data-test='error']"));
-    //     Assert.That(pesan.Text,
-    //         Is.EqualTo("Epic sadface: Username and password do not match any user in this service"));
-    // }
-
     [Test]
     public void Test2()
     {
-        driver.Manage().Window.Size = new System.Drawing.Size(1920, 1080);
-        driver.Navigate().GoToUrl("http://localhost:4200");
-        driver.FindElement(RelativeBy.WithLocator(By.TagName("textarea"))
+        GetDriver().FindElement(RelativeBy.WithLocator(By.TagName("textarea"))
         .Below(By.Id("full-name"))).SendKeys("something important");
 
-        driver.FindElements(RelativeBy.WithLocator(By.CssSelector("input[type='checkbox']"))
+        GetDriver().FindElements(RelativeBy.WithLocator(By.CssSelector("input[type='checkbox']"))
         .Below(By.Id("full-name")))[1].Click();
     }
 
     [Test]
     public void TestWithAssertion_1Data()
     {
-        driver.Manage().Window.Size = new System.Drawing.Size(1920, 1080);
-        driver.Navigate().GoToUrl("http://localhost:4200");
-        driver.FindElement(By.Id("full-name")).SendKeys("Erick Kurniawan");
+        GetDriver().FindElement(By.Id("full-name")).SendKeys("Erick Kurniawan");
         //driver.FindElement(By.Id("add-btn")).Click();
 
-        var addButton = driver.FindElement(By.Id("add-btn"));
-        ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].click();", addButton);
+        var addButton = GetDriver().FindElement(By.Id("add-btn"));
+        ((IJavaScriptExecutor)GetDriver()).ExecuteScript("arguments[0].click();", addButton);
 
-        var totalPrice = driver.FindElement(By.CssSelector("tfoot tr th:nth-child(3)"));
+        var totalPrice = GetDriver().FindElement(By.CssSelector("tfoot tr th:nth-child(3)"));
         //Assert.AreEqual("$200.00", totalPrice.Text, "Total price is correct");
         Assert.That(totalPrice.Text, Is.EqualTo("$100.00"), "Total price is correct");
     }
@@ -70,10 +38,10 @@ public class SimpleTest
     [TestCase("Joko Susilo", "$100.00")]
     public void TestWithAssertion_2Data(string nama, string total)
     {
-        driver.FindElement(By.Id("full-name")).SendKeys(nama);
-        driver.FindElement(By.Id("add-btn")).Click();
+        GetDriver().FindElement(By.Id("full-name")).SendKeys(nama);
+        GetDriver().FindElement(By.Id("add-btn")).Click();
 
-        var totalPrice = driver.FindElement(By.CssSelector("tfoot tr th:nth-child(3)"));
+        var totalPrice = GetDriver().FindElement(By.CssSelector("tfoot tr th:nth-child(3)"));
         //Assert.AreEqual("$200.00", totalPrice.Text, "Total price is correct");
         Assert.That(totalPrice.Text, Is.EqualTo(total), "Total price is correct");
     }
@@ -81,24 +49,22 @@ public class SimpleTest
     [Test]
     public void TestUseDoubleClick()
     {
-        var nameinput = driver.FindElement(By.Id("full-name"));
+        var nameinput = GetDriver().FindElement(By.Id("full-name"));
         nameinput.SendKeys("Erick Kurniawan");
 
-        var addButton = driver.FindElement(By.Id("add-btn"));
-        var actions = new Actions(driver);
+        var addButton = GetDriver().FindElement(By.Id("add-btn"));
+        var actions = new Actions(GetDriver());
         actions.DoubleClick(addButton).Perform();
 
-        Assert.That(driver.FindElement(By.Id("full-name_validation-error")).Displayed,
+        Assert.That(GetDriver().FindElement(By.Id("full-name_validation-error")).Displayed,
             Is.True, "Validation error is displayed");
     }
 
     [Test]
     public void Test_UsingClearText()
     {
-        driver.Manage().Window.Size = new System.Drawing.Size(1920, 1080);
-        driver.Navigate().GoToUrl("http://localhost:4200");
 
-        var noteTextArea = driver.FindElement(By.Id("notes"));
+        var noteTextArea = GetDriver().FindElement(By.Id("notes"));
         noteTextArea.SendKeys("something important");
         noteTextArea.Clear();
         noteTextArea.SendKeys("replace text after clear");
@@ -107,23 +73,20 @@ public class SimpleTest
             Is.EqualTo("replace text after clear"));
     }
 
-    [Test]
-    public void Test_UsingUpload()
-    {
-        var photoInput = driver.FindElement(By.Id("photo"));
-        photoInput.SendKeys(Path.GetFullPath(Path.Join("Data", "photo.png")));
+    // [Test]
+    // public void Test_UsingUpload()
+    // {
+    //     var photoInput = GetDriver().FindElement(By.Id("photo"));
+    //     photoInput.SendKeys(Path.GetFullPath(Path.Join("Data", "photo.png")));
 
-        Assert.That(photoInput.GetAttribute("value"),
-            Is.EqualTo(Path.GetFullPath(Path.Join("Data", "photo.png"))));
-    }
+    //     Assert.That(photoInput.GetAttribute("value"),
+    //         Is.EqualTo(Path.GetFullPath(Path.Join("Data", "photo.png"))));
+    // }
 
     [Test]
     public void Test_UsingPressing()
     {
-        driver.Manage().Window.Size = new System.Drawing.Size(1920, 1080);
-        driver.Navigate().GoToUrl("http://localhost:4200");
-
-        var includeLuchDropdown = driver.FindElement(By.Id("include-lunch"));
+        var includeLuchDropdown = GetDriver().FindElement(By.Id("include-lunch"));
         var includeLunchSelect = new SelectElement(includeLuchDropdown);
         includeLunchSelect.SelectByText("Yes");
 
